f9683bb71357d65c043264de0be328f514a26edf
katie lahani <katielahani@gmail.com>
2015-10-12 14:37:00 +0100

    Implemented Devise for users

--- Gemfile
+++ Gemfile
@@ -7,9 +7,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
-gem 'jbuilder', '~> 2.0'
+gem 'jbuilder', '~> 2.0'		
 gem 'sdoc', '~> 0.4.0'
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.5.2.rc2'
 
 group :development, :test do
      gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,3 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+# Required for heroku
+# Note to set this to your actual host
+  config.action_mailer.default_url_options = { :host => 'http://kbc-pinteresting-commits.herokuapp.com/' }
+end
